8f7feaa9c5c913a3b5d02f9a316655b5d28a0946
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-05-31 17:19:28 -0300

    :sparkles: feat: function get_unique_job_types

--- src/insights.py
+++ src/insights.py
@@ -1,3 +1,6 @@
+from src.jobs import read
+
+
 def get_unique_job_types(path):
     """Checks all different job types and returns a list of them
 
@@ -13,7 +16,11 @@ def get_unique_job_types(path):
     list
         List of unique job types
     """
-    return []
+    data = read(path)
+    types_job = set()
+    for job in data:
+        types_job.add(job["job_type"])
+    return [types for types in types_job]
 
 
 def filter_by_job_type(jobs, job_type):
