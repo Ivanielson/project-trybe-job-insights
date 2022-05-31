686da940c92ff88384ece8480c7d01ea54e2c49c
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-05-31 15:42:00 -0300

    :sparkles: feat: function read

--- src/jobs.py
+++ src/jobs.py
@@ -1,3 +1,4 @@
+import csv
 from functools import lru_cache
 
 
@@ -15,4 +16,6 @@ def read(path):
     list
         List of rows as dicts
     """
-    return []
+    with open(path, encoding="utf-8") as file:
+        jobs_reader = csv.DictReader(file, delimiter=",", quotechar='"')
+        return [jobs for jobs in jobs_reader]
