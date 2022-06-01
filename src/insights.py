e7b975e49e74f75cbc9fbe09b361c2233ea8c77b
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-06-01 16:16:06 -0300

    :recycle: refactor: function filter_by_salary_range

--- src/insights.py
+++ src/insights.py
@@ -186,4 +186,11 @@ def filter_by_salary_range(jobs, salary):
     list
         Jobs whose salary range contains `salary`
     """
-    return []
+    list_jobs = []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                list_jobs.append(job)
+        except ValueError:
+            pass
+    return list_jobs
