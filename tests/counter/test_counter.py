9ce4447aa89eb830d5262b7a38dd5b2b1a5df1e7
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-06-01 16:19:27 -0300

    :test_tube: test: Implement tests for count_occurrences function

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,21 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
+from unittest.mock import mock_open, patch
 
 
 def test_counter():
-    pass
+    file_fake = """
+        Uma vez Flamengo, sempre Flamengo
+        Flamengo sempre eu hei de ser
+        É o meu maior prazer vê-lo brilhar
+        Seja na terra, seja no mar
+        Vencer, vencer, vencer
+        Uma vez Flamengo, Flamengo até morrer
+        Na regata ele me mata, me maltrata
+        Me arrebata, que emoção no coração
+        Consagrado no gramado, sempre amado
+        Mais cotado no Fla-Flu, é o ai Jesus
+    """
+    with patch("builtins.open", mock_open(read_data=file_fake)):
+        assert count_ocurrences("dummy", "Flamengo") == 5
+        assert count_ocurrences("dummy", "Jesus") == 1
+        assert count_ocurrences("dummy", "vencer") == 3
